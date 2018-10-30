c054020a15e06b60a283afc84e8039944b939ef5
Spencer Croy <spencerccroy@gmail.com>
2018-10-30 22:13:13 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,15 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz
-
-end
+def fizzbuzz(num)
+  if num % 15 == 0 
+    "FizzBuzz"
+  end
+  elsif num % 5 == 0
+    "Buzz"
+  end
+  elsif num % 3 == 0
+    "Fizz"
+  end
+  else
+  nil
+  end
